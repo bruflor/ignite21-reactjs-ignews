9229e102e32c29e61d307ddee19e453ef21127c8
Bruna Flor <brn.flor@gmail.com>
2023-02-15 17:09:42 +0000

    basic config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.scss";
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
       <h1>Hello </h1>
     </>
   );
