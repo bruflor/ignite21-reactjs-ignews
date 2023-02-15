35669ef9c92524da8b80fad13f0ff7322299335b
Bruna Flor <brn.flor@gmail.com>
2023-02-15 17:09:52 +0000

    wip:component header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
