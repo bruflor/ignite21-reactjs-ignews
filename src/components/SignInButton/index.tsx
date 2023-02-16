680c5b7038fe7d1afa2a52891d4232172a14f00a
Bruna Flor <brn.flor@gmail.com>
2023-02-16 12:23:13 +0000

    home done

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -3,7 +3,7 @@ import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
 
 export function SignButton() {
-  const isUserLoggedIn = true;
+  const isUserLoggedIn = false;
   return isUserLoggedIn ? (
     <button className={styles.signInButton}>
       <FaGithub color="#04d361" />
