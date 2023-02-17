0d03a17ab5d12d43dc68a63cb6668967c84fe110
Bruna Flor <brn.flor@gmail.com>
2023-02-17 16:00:40 +0000

    subscribe button

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,5 @@
+import { api } from "@/services/api";
+import { getStripeJs } from "@/services/stripe-js";
 import { signIn, useSession } from "next-auth/react";
 import styles from "./styles.module.scss";
 
@@ -8,15 +10,30 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const { data: session } = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       signIn("github");
       return;
     }
+
+    try {
+      const response = await api.post("subscribe");
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      stripe?.redirectToCheckout({ sessionId });
+    } catch (err: any) {
+      alert(err.message);
+    }
   }
 
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe Now
     </button>
   );
--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -7,11 +7,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const session = await getSession({ req });
 
     const stripeCustomer = await stripe.customers.create({
-        email: session?.user?.email || undefined
+      email: session?.user?.email || undefined,
     });
-    
+
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: {stripeCustomer.id},
+      customer: stripeCustomer.id,
       payment_method_types: ["card"],
       billing_address_collection: "required",
       line_items: [{ price: "price_1Mc6s9DnHiKpoTei2NrPAzOh", quantity: 1 }],
@@ -20,7 +20,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       success_url: process.env.STRIPE_SUCESS_URL as string,
       cancel_url: process.env.STRIPE_CANCEL_URL as string,
     });
-    return res.status(200).json({sessionId: stripeCheckoutSession.id})
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id });
   } else {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method not allowed");
