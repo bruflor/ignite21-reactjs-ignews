3d844e0a6c6c15ee4d66d61853546461d88cc060
Bruna Flor <brn.flor@gmail.com>
2023-02-17 15:31:11 +0000

    wip: subscribe

---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import { stripe } from "../../services/stripe";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    const session = await getSession({ req });
+
+    const stripeCustomer = await stripe.customers.create({
+        email: session?.user?.email || undefined
+    });
+    
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: {stripeCustomer.id},
+      payment_method_types: ["card"],
+      billing_address_collection: "required",
+      line_items: [{ price: "price_1Mc6s9DnHiKpoTei2NrPAzOh", quantity: 1 }],
+      mode: "subscription",
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCESS_URL as string,
+      cancel_url: process.env.STRIPE_CANCEL_URL as string,
+    });
+    return res.status(200).json({sessionId: stripeCheckoutSession.id})
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method not allowed");
+  }
+};
