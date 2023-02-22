7da6dfd145b99437b6c0193dba415c617f4da4c5
Bruna Flor <brn.flor@gmail.com>
2023-02-22 14:13:16 +0000

    wip: header routes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,24 @@
 import { SignButton } from "../SignInButton";
+import Link from 'next/link'
+
 import styles from "./styles.module.scss";
+
+
 export default function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
+        
+          <Link href='/'>
           <a className={styles.active}>Home</a>
+          </Link>
+         
+          <Link href='/posts'>
           <a>Posts</a>
+          </Link>
+        
         </nav>
 
         <SignButton />
